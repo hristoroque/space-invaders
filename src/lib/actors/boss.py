4ae0da2da6ef44f879ac0c6b52eb2f49e1970ade
Amilcar Maximo Laura Canaza <alaurac@unsa.edu.pe>
2020-09-23 00:24:10 -0500

    reducir aun mas el intervalo de ataque de boss

--- src/lib/actors/boss.py
+++ src/lib/actors/boss.py
@@ -42,7 +42,7 @@ class Boss(Actor):
     def update(self, delta_time):
         super(Boss, self).update(delta_time)
         if(self.timer_shoot == 0):
-            self.timer_shoot = 300
+            self.timer_shoot = 200
             self.shoot()
         self.timer_shoot = self.timer_shoot - 1
 
